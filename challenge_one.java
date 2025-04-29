79f5caa66483929f7a6f7bef2b1f636d8a0c368b
Mulama05 <bonimechii80@gmail.com>
2025-04-29 15:01:57 +0300

    Create challenge_one.java

---
+++ challenge_one.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class greet {
+    void Greet() {
+        System.out.println("Welcome to Kenya!");
+    }
+    public static void main(String[] args) {
+        greet g = new greet();
+        g.Greet();
+    }
+}
